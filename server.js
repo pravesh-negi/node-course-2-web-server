083f5d0f53f3ba4ad7f8743338124280a997e364
pravesh negi <14negi.pravesh@gmail.com>
2019-11-30 23:49:13 +0530

    project files

--- server.js
+++ server.js
@@ -93,6 +93,12 @@ app.get('/about',(req,res)=>{
     });
 });
 
+app.get('/projects',(req,res)=>{ 
+    res.render('projects.hbs',{
+        pageTitle:'projects' 
+    });
+});
+
 // send back json with errormessage 
 app.get('/bad',(req,res)=>{
     res.send({
